403c6765a5c7bb032ee15e6a371e8046889b6023
Martin Fahlström <makrill88@gmail.com>
2024-03-20 09:46:56 +0100

    fixed naming convention

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import './App.css'
 // import EditUser from './Components/EditName'
 // import RandomNumber from './RandomNumber'
 // import Fruits from './Components/FruitList.jsx'
-import { RenderCitys } from './Components/Citys'
-import Athletes from './Components/sportsPeople'
+// import { RenderCitys } from './Components/Citys'
+import Athletes from './Components/SportsPeople'
 
 
 function App() {
